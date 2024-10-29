48554bad7edd02fd74158594cf2d8253d5242ae5
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-10-29 12:35:11 -0700

    create object from cart database

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -12,7 +12,13 @@ const URL = "https://audiophile-e-commerce-backend.onrender.com";
 const cartResponse = await fetch(`${URL}/getData`);
 // console.log(await cartResponse.json());
 const cartArray = await cartResponse.json();
-const cart = cartArray.data.reduce((a, v) => ({ ...a, [v.product]: v }), {});
+const cart = cartArray.data.reduce(
+  (a, v) => ({
+    ...a,
+    [v.product]: { amount: v.amount, price: v.price, image: v.image },
+  }),
+  {}
+);
 console.log(cart);
 if (!("cart" in localStorage)) {
   localStorage.setItem("cart", JSON.stringify({}));
